f687c19937d3c8e6e5c6f67ca15479d603aeb4ad
WhoDaPenguin <61098297+WhoDaPenguin@users.noreply.github.com>
2023-09-20 13:52:40 -0400

    Takes grid in as user input correctly

--- word_grid_solution_finder.py
+++ word_grid_solution_finder.py
@@ -2,19 +2,39 @@ import functools
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-width = input("Enter the grid's width: ")
-height = input("Enter the grid's height: ")
+width = 0
+height = 0
 grid = []
-
-# convert list of characters to string
-# from Geeksforgeeks
-def convert(s):
-    # Using reduce to join the list s to string
-    str1 = functools.reduce(lambda x,y : x+y, s)
-    return str1
 
 def print_grid():
     for row in grid:
         for letter in row:
             print(letter, end=" ")
         print()
+
+# open "input.txt" and initialize  the user-input grid
+with open("input.txt", 'r') as input_file:
+
+    # read in every line from "input.txt"
+    lines = input_file.readlines()
+
+    # strip each line
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip()
+
+    # read the first line to find the width and height
+    width_and_height = lines[0].split(' ')
+    width = int(width_and_height[0])
+    height = int(width_and_height[1])
+
+    # initialize the grid with empty rows according to the width and height
+    grid = [['#' for _ in range(width)] for _ in range(height)]
+
+    # populate the grid according to the input
+    row = 1
+    while row <= height:
+        input_row = lines[row].split(' ')
+        for i in range(len(input_row)):
+            grid[row - 1][i] = input_row[i]
+        row += 1
+
